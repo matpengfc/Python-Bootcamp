769ffe8d67b10b93112d3a56c8e4eeb7ab30403f
Amir Husaini <amir.husaini89@gmail.com>
2022-02-12 19:59:13 +0800

    optimize Day 2 script

--- Day_2_Tip_Calculator.py
+++ Day_2_Tip_Calculator.py
@@ -3,15 +3,15 @@
 # Round the result to 2 decimal places = 33.60
 
 print("Welcome to the tip calculator.")
-total_bill = input("What was the total bill? $")
-tip_perc   = input("What percentage tip would you like to give? 10, 12, or 15? ")
-party_size = input("How many people to plit the bill? ")
+total_bill = float(input("What was the total bill? $"))
+tip_perc   = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+party_size = int(input("How many people to plit the bill? "))
 
-total_bill_float = float(total_bill)
-tip_perc_int     = int(tip_perc)
-party_size_int   = int(party_size)
+# total_bill_float = float(total_bill)
+# tip_perc_int     = int(tip_perc)
+# party_size_int   = int(party_size)
 
-split_amount = total_bill_float * (tip_perc_int / 100.0 + 1.0) / party_size_int
+split_amount = total_bill * (tip_perc / 100.0 + 1.0) / party_size
 split_amount_round = round(split_amount,2)
 
 print(f"Each person should pay: ${split_amount_round}")
