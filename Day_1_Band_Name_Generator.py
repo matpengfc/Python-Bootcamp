cf895abaf79033df3ea2b675a331471b289864a1
Amir Husaini <amir.husaini89@gmail.com>
2022-02-12 20:36:55 +0800

    clean up Day 1 script and rename cwd reference script

--- Day_1_Band_Name_Generator.py
+++ Day_1_Band_Name_Generator.py
@@ -1,36 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Background
-
-# Using city and pet to create a band name generator
-
-# ## Code
-
-# In[1]:
-
-
 # Create a greeting
 print("Welcome to the Band Name Generator")
-
-
-# In[2]:
-
-
 # Ask the user for the city that they grew up in.
 city = input("What's name of the city you grew up in? \n")
-
-
-# In[3]:
-
-
 # Ask the user for the name of a pet
 pet = input("What's your pet's name? \n")
-
-
-# In[4]:
-
-
 # Combine the name of their city and pet and show them their band name
 print("Your band name could be " + city + " " + pet)
 
