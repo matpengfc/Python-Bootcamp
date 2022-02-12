f5d11d71d2b6ac00fbe453a10ea8a79cfb06c9c4
Amir Husaini <amir.husaini89@gmail.com>
2022-02-12 20:08:04 +0800

    enhance from round to 2f decimal format

--- Day_2_Tip_Calculator.py
+++ Day_2_Tip_Calculator.py
@@ -13,7 +13,8 @@ party_size = int(input("How many people to plit the bill? "))
 
 split_amount = total_bill * (tip_perc / 100.0 + 1.0) / party_size
 split_amount_round = round(split_amount,2)
+split_amount_2f_format = "{:.2f}".format(split_amount) # better presentation of decimal numbers
 
-print(f"Each person should pay: ${split_amount_round}")
+print(f"Each person should pay: ${split_amount_2f_format}")
 
 
