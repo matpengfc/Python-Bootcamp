dfe33f7917d5f3458fd72a43387eae1c6af2bde0
Amir Husaini <amir.husaini89@gmail.com>
2022-02-12 17:02:48 +0800

    Change from ipynb to py

---
+++ Updating_script.py
@@ -0,0 +1,8 @@
+import os
+cwd = os.getcwd()
+print(cwd)
+
+# running python in PowerShell
+# python Updating_script.py
+# jupyter nbconvert -- to script Day*.ipynb ==> convert ipynb with wildcard
+# Remove-Item test.* ==> remove test file
