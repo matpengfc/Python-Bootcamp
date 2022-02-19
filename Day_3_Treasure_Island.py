17c4a2eba13c7c888e753195119ca6de1b5efce5
Amir Husaini <amir.husaini89@gmail.com>
2022-02-19 12:29:43 +0800

    Updating Day 3

---
+++ Day_3_Treasure_Island.py
@@ -0,0 +1,51 @@
+# Day 3
+# ASCII art at https://ascii.co.uk/art
+
+from turtle import left
+
+
+print('''
+                                                                   ,-,
+                                                             _.-=;~ /_
+                                                          _-~   '     ;.
+                                                      _.-~     '   .-~-~`-._
+                                                _.--~~:.             --.____88
+                              ____.........--~~~. .' .  .        _..-------~~
+                     _..--~~~~               .' .'             ,'
+                 _.-~                        .       .     ` ,'
+               .'                                    :.    ./
+             .:     ,/          `                   ::.   ,'
+           .:'     ,(            ;.                ::. ,-'
+          .'     ./'.`.     . . /:::._______.... _/:.o/
+         /     ./'. . .)  . _.,'               `88;?88|
+       ,'  . .,/'._,-~ /_.o8P'                  88P ?8b
+    _,'' . .,/',-~    d888P'                    88'  88|
+ _.'~  . .,:oP'        ?88b              _..--- 88.--'8b.--..__
+:     ...' 88o __,------.88o ...__..._.=~- .    `~~   `~~      ~-._ Seal _.
+`.;;;:='    ~~            ~~~                ~-    -       -   -
+''')
+print("Welcome to Fox Island.")
+print("Your mission is to find the treasure.")
+
+level_1 = input('You\'re at a crossroad. Where do you want to go> Type "left" or "right"\n ').lower()
+if level_1 != "left":
+    print("You fell into a hole. Game Over.")
+else:
+    level_2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n')
+    if level_2.lower() != "wait":
+        print("You get attacked by an angry trout. Game Over.")
+    else:
+        level_3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+        if level_3.lower() == "red":
+            print("It's a room full of fire. Game Over.")
+        elif level_3.lower() == "blue":
+            print("You enter a room of beasts. Game Over.")
+        elif level_3.lower() == "yellow":
+            print("You found the treasure! You Win!")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+
+print("The End")
+    
+
+
