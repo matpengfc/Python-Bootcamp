697fe59cfa5281ced44170a5bb2b92fc535e1da2
Amir Husaini <amir.husaini89@gmail.com>
2022-03-27 12:27:00 +0800

    Updated Day 6 - with comments script

--- Day_6_Reeborg_World.py
+++ Day_6_Reeborg_World.py
@@ -3,21 +3,21 @@
 # Angela Yu solution
 # This code only work in Reeborg's World
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
-# to solve a extreme case bug - robot turn in circle in infinity loop in open space(turn right all the time), push robot to the wall
-while front_is_clear():
-    move()
-turn_left()
+# # to solve a extreme case bug - robot turn in circle in infinity loop in open space(turn right all the time), push robot to the wall
+# while front_is_clear():
+#     move()
+# turn_left()
 
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clera():
-        move()
-    else:
-        turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clera():
+#         move()
+#     else:
+#         turn_left()
